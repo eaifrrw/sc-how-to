3a5b3be70438abdfb0e7cdf86cd93c276397924d
ed-ntim <egasu24@gmail.com>
2025-09-16 16:38:41 +0100

    added configuration for sphinx

---
+++ docs/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ICTP-EAIFR Cluster User Guide'
+copyright = '2025, Edward Ntim Gasu, Ph.D'
+author = 'Edward Ntim Gasu, Ph.D'
+release = '0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+extensions = [
+    "myst_parser",
+]
+
+# Allow .md files
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
---
+++ docs/index.rst
@@ -0,0 +1,28 @@
+.. ICTP-EAIFR Cluster User Guide documentation master file, created by
+   sphinx-quickstart on Tue Sep 16 16:13:46 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ICTP-EAIFR Cluster User Guide documentation
+===========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+   cluster_specifications
+   data_transfer
+   job_status
+   linux_command_line
+   more_info
+   parallel_jobs
+   remote_login
+   serial_jobs
+   software_environments
+   text_editor
